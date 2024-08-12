70c5ea409b5922b41b6b11e547ac50209b307f6c
jamilya-dev <omg.groups@yandex.ru>
2024-08-12 22:35:10 +0300

    задание 15

--- js/script.js
+++ js/script.js
@@ -9,19 +9,19 @@ const DomElement = function (selector, height, width, bg, fontSize) {
   this.createElement = function (selector) {
     let elem = '';
     if (selector.startsWith('.')) {
-      elem = document.createElement('div')
-      elem.classList.add(selector.substring(1))
+      elem = document.createElement('div');
+      elem.classList.add(selector.substring(1));
     } else if (selector.startsWith('#')) {
-      elem = document.createElement('p')
-      elem.setAttribute('id', selector.substring(1))
+      elem = document.createElement('p');
+      elem.setAttribute('id', selector.substring(1));
     }
-    elem.style.cssText = 'height:' + height + 'px; width:' + width + 'px; background-color:' + bg + '; font-size:' + fontSize + 'px;'
-    elem.textContent = 'Text'
-    document.querySelector('.container').append(elem)
+    elem.style.cssText = 'height:' + height + 'px; width:' + width + 'px; background-color:' + bg + '; font-size:' + fontSize + 'px;';
+    elem.textContent = 'Text';
+    document.querySelector('.container').append(elem);
   }
 }
 
-const createBlock = new DomElement('.block', 100, 100, 'red', 16)
-createBlock.createElement(createBlock.selector)
+const createBlock = new DomElement('.block', 100, 100, 'red', 16);
+createBlock.createElement(createBlock.selector);
 
 
